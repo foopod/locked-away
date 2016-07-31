ee0f9af834c92b488a248939f61c16a908f26409
Jonathon Shields <jonathonshields@gmail.com>
2016-07-31 21:43:54 +1200

    init commit

---
+++ entries.js
@@ -0,0 +1,15 @@
+var entries = [
+    {
+        "date" : "2018-05-29",
+        "entry" : "Jeremy messaged me again today, said it was good to see me at the poetry meetup. I wonder what he is up to these days, I should have asked, its been a while. I wonder if he is still seeing that girl Tammy, she was really cool... and not just because she complimented my skirt."
+    },
+    {
+        "date" : "2018-05-26",
+        "entry" : "Wow, Bill thanked me today for lending him my jacket months ago. Can't believe he even remembers, let alone be important enough to tell me. Weird story actually, he needed a rock'n'roll sorta jacket to wear to a gig, but none of his guys friends had anything... Kelly saves the day again"
+    },
+    {
+        "date" : "2018-05-12",
+        "entry" : ""
+    }
+    
+              ];
